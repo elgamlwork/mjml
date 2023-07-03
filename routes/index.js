299fbcf1cf6595c7fd97cbe8cc06e934e046fb5c
elgamlinfo <elgaml.info@gmail.com>
2023-07-03 17:12:53 +0300

    ff

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,10 @@ router.post("/", function (req, res, next) {
                 <mj-head>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
+                                     
                 <mj-font name="Cairo" href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000&display=swap" />
 
                 <mj-style>
-                        @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000&display=swap');
                         * { 
                             padding: 0px;
                             margin: 0px;
